982c750a15435f4874f41a04bd42d55d9a981b38
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-05-31 18:22:44 +0000

    Revert "add google tage"
    This reverts commit 9e1f297c6d17556e08e6b66ce6e5e617581c904b.

--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import ReactGA from 'react-ga'
 
 import { AppContextProvider } from 'context/app'
 import { AuthContextProvider } from 'context/auth'
@@ -8,23 +7,16 @@ import { ApiContextProvider } from 'context/api'
 
 import Router from 'routes/register'
 
-const App = () => {
-  React.useEffect(() => {
-    ReactGA.initialize('GTM-MWRGK65')
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }, [])
-
-  return (
-    <BrowserRouter>
-      <AppContextProvider>
-        <ApiContextProvider>
-          <AuthContextProvider>
-            <Router />
-          </AuthContextProvider>
-        </ApiContextProvider>
-      </AppContextProvider>
-    </BrowserRouter>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <AppContextProvider>
+      <ApiContextProvider>
+        <AuthContextProvider>
+          <Router />
+        </AuthContextProvider>
+      </ApiContextProvider>
+    </AppContextProvider>
+  </BrowserRouter>
+)
 
 export default App
