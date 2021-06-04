e52dd6cbb7ef57e5524aff71ef9389799c5a6df7
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-06-04 13:05:17 +0000

    add google tag

--- src/container/App.js
+++ src/container/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import ReactGA from 'react-ga'
 
 import { AppContextProvider } from 'context/app'
 import { AuthContextProvider } from 'context/auth'
@@ -7,16 +8,22 @@ import { ApiContextProvider } from 'context/api'
 
 import Router from 'routes/register'
 
-const App = () => (
-  <BrowserRouter>
-    <AppContextProvider>
-      <ApiContextProvider>
-        <AuthContextProvider>
-          <Router />
-        </AuthContextProvider>
-      </ApiContextProvider>
-    </AppContextProvider>
-  </BrowserRouter>
-)
+const App = () => {
+  React.useEffect(() => {
+    ReactGA.initialize('GTM-MWRGK65')
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
 
+  return (
+    <BrowserRouter>
+      <AppContextProvider>
+        <ApiContextProvider>
+          <AuthContextProvider>
+            <Router />
+          </AuthContextProvider>
+        </ApiContextProvider>
+      </AppContextProvider>
+    </BrowserRouter>
+  )
+}
 export default App
