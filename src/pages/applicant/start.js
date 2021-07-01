c3e4e8cd2cdbfa6103b8f8f3e20e066331de33ce
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-07-01 07:47:00 +0000

    update

--- src/pages/applicant/start.js
+++ src/pages/applicant/start.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Flex, Text } from '@chakra-ui/react'
 import { Layout, StepOne } from 'components/ApplicationSteps'
 import useApp from 'context/app'
 
@@ -7,6 +10,9 @@ const Start = props => {
   React.useEffect(() => {
     setStep(1)
   }, [setStep])
+
+  const { history } = props
+
   return (
     <Layout
       title='Application Process'
@@ -15,8 +21,25 @@ const Start = props => {
       {...props}
     >
       <StepOne {...props} />
+      <Flex mt={4} justify='center'>
+        <Text
+          color='gcu.100'
+          cursor='pointer'
+          fontWeight='bold'
+          textDecor='underline'
+          onClick={_ => history.push('/applicant/login')}
+        >
+          Login
+        </Text>
+        <Text d={{ base: 'none', lg: 'block' }} ml={3}>
+          to resume pending applications
+        </Text>
+      </Flex>
     </Layout>
   )
 }
 
+Start.propTypes = {
+  history: PropTypes.object.isRequired
+}
 export default Start
